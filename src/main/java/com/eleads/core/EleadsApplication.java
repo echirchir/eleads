914ac30e2609591098a916eacd453e5d33bce77e
sdafrica <elisha.chirchir@softwaredynamics.co>
2020-01-14 06:12:03 -0600

    Initial commit for eleads module
    Add spring boot scaffolding with maven
    dependencies as initial project starting template
    Configure postgres database connector

---
+++ src/main/java/com/eleads/core/EleadsApplication.java
@@ -0,0 +1,23 @@
+package com.eleads.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class EleadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleadsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+
+		return "home";
+	}
+
+
+}
---
+++ src/test/java/com/eleads/core/EleadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleads.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
