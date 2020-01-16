396b95b35b67fe5c8e082f5e3240baa6b6d8d4e9
sdafrica <elisha.chirchir@softwaredynamics.co>
2020-01-16 08:32:52 -0600

    Tested hotels resource controller for REST API

--- src/main/java/com/eleads/core/controllers/HotelsController.java
+++ src/main/java/com/eleads/core/controllers/HotelsController.java
@@ -2,14 +2,7 @@ package com.eleads.core.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 @RestController
 public class HotelsController{
-
-    @GetMapping("/myhotel")
-    public String getAll() {
-        return "dashboard";
-    }
     
 }
--- src/main/java/com/eleads/core/models/Hotel.java
+++ src/main/java/com/eleads/core/models/Hotel.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Hotel{
@@ -12,7 +13,9 @@ public class Hotel{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @NotNull
     private String roomtype;
+    @NotNull
     private double price;
 
     public Hotel(int id, String roomtype, double price) {
